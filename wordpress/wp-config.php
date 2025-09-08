aac5180d8b26a2961d3868c0bcd498147f364c8c
Khardiata.Diop <khardiata.diop@outlook.com>
2025-09-08 09:43:27 +0000

    wp config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'khardiatadiop_Iinit_WordPress_Remake' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'khardiatadiop' );
 
 /** Database password */
 define( 'DB_PASSWORD', 'password_here' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db.3wa.io' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
